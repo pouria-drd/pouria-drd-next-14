7b48d8aee46055c1cdc530c01b02da99f6b27bc7
pouria-drd <pour1a.dra80@gmail.com>
2024-08-17 11:06:20 +0330

    added type writer (typed.js)

--- components/ui/index.ts
+++ components/ui/index.ts
@@ -5,3 +5,4 @@ export { default as PouriaLogo } from "./logo/PouriaLogo";
 export { default as MenuButton } from "./button/MenuButton";
 export { default as CloseButton } from "./button/CloseButton";
 export { default as AppVersion } from "./app-version/AppVersion";
+export { default as Typewriter } from "./type-writer/Typewriter";
---
+++ components/ui/type-writer/Typewriter.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import Typed from "typed.js";
+import { useRef, useEffect } from "react";
+
+interface TypewriterProps {
+    texts: string[];
+}
+
+const Typewriter = (typewriterProps: TypewriterProps) => {
+    // Create reference to store the DOM element containing the animation
+    const el = useRef(null);
+
+    useEffect(() => {
+        const typed = new Typed(el.current, {
+            strings: typewriterProps.texts,
+            typeSpeed: 75,
+            loop: true,
+            cursorChar: "",
+        });
+
+        return () => {
+            // Destroy Typed instance during cleanup to stop animation
+            typed.destroy();
+        };
+    }, []);
+
+    return <span ref={el} />;
+};
+
+export default Typewriter;
