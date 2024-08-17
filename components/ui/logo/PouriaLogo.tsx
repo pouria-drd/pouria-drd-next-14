d3c4cfa79d191a42b713a0b2d2ae95361ea84b25
pouria-drd <pour1a.dra80@gmail.com>
2024-08-17 09:49:25 +0330

    added link to logo

--- components/ui/logo/PouriaLogo.tsx
+++ components/ui/logo/PouriaLogo.tsx
@@ -1,13 +1,14 @@
+import Link from "next/link";
 import { PdIcon } from "@/components/icons";
 
 const PouriaLogo = () => {
     return (
-        <div className="flex text-drd-neutral-40 truncate">
+        <Link href="/" className="flex text-drd-neutral-40 truncate">
             <PdIcon className="text-drd-primary" />
             <p className="text-2xl font-bold uppercase mx-0.5 ml-1 mt-2">
                 Pouria DRD
             </p>
-        </div>
+        </Link>
     );
 };
 
