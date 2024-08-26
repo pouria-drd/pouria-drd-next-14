5913afb7670d2ede8049a01ce35099b64d36c909
pouria-drd <pour1a.dra80@gmail.com>
2024-08-26 10:17:41 +0330

    remove home page test divs and changed footer components

--- app/page.tsx
+++ app/page.tsx
@@ -2,17 +2,6 @@ function HomePage() {
     return (
         <section className="flex flex-col gap-2">
             <h1>Hello World</h1>
-
-            <div className="size-60 bg-drd-neutral-80 rounded-xl"></div>
-            <div className="size-60 bg-drd-neutral-80 rounded-xl"></div>
-            <div className="size-60 bg-drd-neutral-80 rounded-xl"></div>
-            <div id="about" className="size-60 bg-drd-neutral-80 rounded-xl">
-                about
-            </div>
-            <div className="size-60 bg-drd-neutral-80 rounded-xl"></div>
-            <div className="size-60 bg-drd-neutral-80 rounded-xl"></div>
-            <div className="size-60 bg-drd-neutral-80 rounded-xl"></div>
-            <div className="size-60 bg-drd-neutral-80 rounded-xl"></div>
         </section>
     );
 }
--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -5,9 +5,9 @@ const Footer = () => {
         <footer
             id="footer"
             className="bg-drd-light-primary-0 flex flex-col gap-4 pt-6 app-px">
-            <div className="flex flex-col sm:flex-row items-center justify-between gap-8 sm:gap-px w-full">
+            <div className="flex flex-col sm:flex-row items-center justify-between gap-4 sm:gap-px w-full">
                 <SkillsWriter className="justify-center sm:justify-normal w-full" />
-                <PouriaLogo className="justify-center sm:justify-normal w-full" />
+                <PouriaLogo className="justify-center sm:justify-normal w-full hidden sm:flex" />
                 <SocialCard />
             </div>
             <Copyright />
